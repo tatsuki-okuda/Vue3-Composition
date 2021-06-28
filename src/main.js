bd6e2fb17af644be297c90dc05aece32286a1877
tatasuki-okuda <67616674+tatsuki-okuda@users.noreply.github.com>
2021-06-28 22:21:42 +0900

    composition-api

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueCompositionApi from '@vue/composition-api';
 
-createApp(App).mount('#app')
+
+
+// createApp(App).mount('#app');
+
+
+
+
+const app = createApp(App);
+app.use({
+    VueCompositionApi
+})
+app.mount('#app');
